b94236ae48b4e1fe20cfbf7485b5200a15caa3b3
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-05-06 13:16:59 +0100

    feat: Add necessary methods and properties

--- js/app.js
+++ js/app.js
@@ -1,17 +1,24 @@
 // Enemies our player must avoid
 class Enemy {
-    constructor (sprite) {
-        // Variables applied to each of our instances go here,
-        // we've provided one for you to get started
-    
+    constructor () {
         // The image/sprite for our enemies, this uses
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
+        // TODO: Make speed random
+        this.speed = 10;
+        // TODO: Check spawn locations
+        this.x = 0;
+        // TODO: Check spawn locations
+        this.y = 0;
     }
     update(dt) {
         // You should multiply any movement by the dt parameter
         // which will ensure the game runs at the same speed for
         // all computers.
+
+    }
+    collisionHandler() {
+
     }
     // Draw the enemy on the screen, required method for game
     render() {
@@ -22,11 +29,32 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.sprite = 'images/char-boy.png';
+        this.x = 0;
+        this.y = 0;
+    }
+    update(dt) {
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+    }
+    // Draw the player on the screen, required method for game
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+    handleInput(key) {
+
+    }
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [new Enemy(), new Enemy()];
 // Place the player object in a variable called player
+const player = new Player();
 
 
 
