e3cec8fd1920d567cac744b2d8f523e7b8b620fb
nickbellamy <878712+NickBellamy@users.noreply.github.com>
2018-05-07 16:02:18 +0100

    refactor: Tidy app.js code

--- js/app.js
+++ js/app.js
@@ -1,125 +1,126 @@
-const scorer = {
+// Handles the scoring of the game
+const scoreKeeper = {
     score: 0,
     incrementScore: function() {
-        this.score++;
-        this.showScore();
+        document.querySelector('h2 span').innerHTML = ++this.score;
     },
     resetScore: function() {
         this.score = 0;
-        this.showScore();
-    },
-    showScore: function() {
         document.querySelector('h2 span').innerHTML = this.score;
     }
 }
 
-const gridSize = (() => {
-    const height = 84;
-    const width = 101;
-    return () => ({ y: height, x: width });
-})();
+// Stipulates the size of a tile to aid movement
+const tileSize = { y: 84, x: 101 };
 
-// Enemies our player must avoid
-class Enemy {
-    constructor () {
-        // The image/sprite for our enemies, this uses
-        // a helper we've provided to easily load images
-        this.sprite = 'images/enemy-bug.png';
-        this.speed = (Math.random() * 400) + 200;
-        this.x = -200;
-        this.y = 60 + (gridSize().y * Math.floor(Math.random() * 3));
+/* Character Class */
+
+class Character {
+    constructor(spawnLocation, sprite) {
+        this.sprite = sprite;
+        this.respawn(spawnLocation);
+    }
+    respawn(spawnLocation) {
+        this.x = spawnLocation.x;
+        this.y = spawnLocation.y;
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
+        // update() is needed in engine.js even if not defined
+    }
+}
+
+/* Enemy Class */
+
+class Enemy extends Character {
+    constructor() {
+        const sprite = 'images/enemy-bug.png';
+        super(Enemy.spawnLocation(), sprite);
+        this.speed = Enemy.speed();
+    }
+    collisionHandler() {
+        if (this.x + 50 >= player.x && this.x - tileSize.x < player.x && this.y > player.y && this.y - tileSize.y < player.y) {
+            this.respawn();
+            player.respawn(Player.spawnLocation());
+            scoreKeeper.resetScore();
+        }
+    }
+    respawn() {
+        super.respawn(Enemy.spawnLocation());
+        this.speed = Enemy.speed();
+    }
+    update(dt) {
         this.x += this.speed * dt;
         if (this.x >= 500) {
             this.respawn();
         }
         this.collisionHandler();
     }
-    collisionHandler() {
-        if(this.x + 50 >= player.x && this.x - gridSize().x < player.x && this.y > player.y && this.y - gridSize().y < player.y) {
-            this.respawn();
-            player.respawn();
-            scorer.resetScore();
+    static spawnLocation() {
+        return {
+            x: -tileSize.x, 
+            y: (tileSize.y * Math.floor(Math.random() * 3)) + 60    // 60px offset to center sprite on tile
         }
     }
-    // Draw the enemy on the screen, required method for game
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-    respawn() {
-        this.x = -200;
-        this.y = 60 + (gridSize().y * Math.floor(Math.random() * 3));
-        this.speed = (Math.random() * 400) + 200;
+    static speed() {
+        return (Math.random() * 400) + 200;
     }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-class Player {
+/* Player Class */
+
+class Player extends Character {
     constructor() {
-        this.sprite = 'images/char-boy.png';
-        this.x = 200;
-        this.y = (4 * gridSize().y) + 40;
+        const sprite = 'images/char-boy.png';
+        super(Player.spawnLocation(), sprite);
     }
-    update(dt) {
-        // You should multiply any movement by the dt parameter
-        // which will ensure the game runs at the same speed for
-        // all computers.
-    }
-    // Draw the player on the screen, required method for game
-    render() {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    }
-    respawn() {
-        this.x = 200;
-        this.y = (4 * 84) + 40;
-    }
+    // Move the player on screen
     handleInput(key) {
         switch (key) {
             case 'up' :
-                if (this.y > (0 * gridSize().y) + 40) {
-                    this.y -= gridSize().y;
+                if (this.y >= (1 * tileSize.y) + 40) {
+                    this.y -= tileSize.y;
                 } else {
-                    scorer.incrementScore();
-                    this.respawn();
+                    scoreKeeper.incrementScore();
+                    super.respawn(Player.spawnLocation());
                 }
                 break;
             case 'down' :
-                if (this.y < (4 * gridSize().y) + 40) {
-                    this.y += gridSize().y;
+                if (this.y <= (3 * tileSize.y) + 40) {
+                    this.y += tileSize.y;
                 }
                 break;
             case 'left' :
-                if (this.x > (2 * gridSize().x) - 200) {
-                    this.x -= gridSize().x;
+                if (this.x >= (1 * tileSize.x)) {
+                    this.x -= tileSize.x;
                 }
                 break;
             case 'right' :
-            if (this.x < (2 * gridSize().x) + 200) {
-                this.x += gridSize().x;
-            }
+                if (this.x <= (3 * tileSize.x)) {
+                    this.x += tileSize.x;
+                }
+        }
+        console.log(this.x + ', ' + this.y);
+    }
+    static spawnLocation() {
+        return {
+            x: (2 * tileSize.x),
+            y: (4 * tileSize.y) + 40    // 40px offset to center sprite on tile
         }
     }
 }
 
+// Place 3 enemy objects in an array called allEnemies
+const allEnemies = [...Array(3)].map(() => new Enemy());
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-const allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 // Place the player object in a variable called player
 const player = new Player();
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// Valid key event listener and handler
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
